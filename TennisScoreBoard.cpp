da169c11983175788f6d5b01c1b0ac871367cfc0
FaizHalde <faiz_84123@yahoo.com>
2015-07-27 10:58:52 +0530

    score Deuce function implemented

--- TennisScoreBoard.cpp
+++ TennisScoreBoard.cpp
@@ -8,6 +8,12 @@ class TennisScoreBoard{
 	int score_F;
 	string score[] = {"0", "15", "30", "40"};
 
+	bool scoreDeuce() {
+		if(score_D == 3 && score_F == 3)
+			return true;
+		return false;
+	}
+
 	TennisScoreBoard(){
 		scoreSequence = "";
 		score_D = 0;
