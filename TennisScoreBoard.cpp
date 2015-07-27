55d4a4d6a543b94d6e21fe99d753887605c1950d
dineshchhabra <test@L-MAA-25004134.paypalcorp.com>
2015-07-27 10:54:25 +0530

    Print score

--- TennisScoreBoard.cpp
+++ TennisScoreBoard.cpp
@@ -2,25 +2,27 @@
 using namespace std;
 
 class TennisScoreBoard{
-	
+
 	string scoreSequence;
 	int score_D;
 	int score_F;
-	string score[] = {"0", "15", "30", "40"};
+	const string score[] = {"0", "15", "30", "40"};
 
 	TennisScoreBoard(){
 		scoreSequence = "";
 		score_D = 0;
 		score_F = 0;
 	}
-	
+
 	string computeScore(){
-		
+
 	}
-	
+
 	public:
 	void displayScore(string scoreSequence){
 		this->scoreSequence = scoreSequence;
+		computeScore();
+		cout << score[score_D] <<" - "<< score[score_F]<<endl;
 	}
-	
+
 };
