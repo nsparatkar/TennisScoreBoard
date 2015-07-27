862cb43a8d0697593395f95a009a3a323fa9b6ac
dineshchhabra <test@L-MAA-25004134.paypalcorp.com>
2015-07-27 11:56:01 +0530

    playerAdvantage removed

--- TennisScoreBoard.cpp
+++ TennisScoreBoard.cpp
@@ -7,36 +7,23 @@ class TennisScoreBoard{
 	int score_D;
 	int score_F;
 	string displayedScore;
-	const string score[5] = {"0", "15", "30", "40"};
-	
+	const string score[5] = {"0", "15", "30", "40","A"};
+
 	void checkAdvantageLost(int i){
-		if(score_D == 4 && scoreSequence.at(i) == 'F')
+		if((score_D == 4 && scoreSequence.at(i) == 'F') || (score_F == 4 && scoreSequence.at(i) == 'D')) {
+		    score_F--;
 			score_D--;
-		if(score_F == 4 && scoreSequence.at(i) == 'D')
-			score_F--;
-	}
-	
-	bool playerAdvantage() {
-		if(score_D == 4 && score_F == 3)
-			return true;
-		if(score_D == 3 && score_F == 4)
-			return true;
-		return false;
-	}
-	
-	void updateScoreBoard(){
-		if(playerAdvantage()) {
-			if(score_D > score_F)
-				displayedScore = "A - ";
-			else
-				displayedScore = " - A";
-		} else {
-			displayedScore += score[score_D];
-			displayedScore += " ";
-			displayedScore += score[score_F];
 		}
 	}
-	
+
+	void updateScoreBoard(){
+
+        displayedScore += score[score_D];
+        displayedScore += " ";
+        displayedScore += score[score_F];
+
+	}
+
 	void computeScore(){
 		for(int i = 0; i < scoreSequence.length(); i++){
 			displayedScore = "";
@@ -57,8 +44,8 @@ class TennisScoreBoard{
 		score_D = 0;
 		score_F = 0;
 		displayedScore = "0 - 0";
-	}	
-		
+	}
+
 	void displayScore(string scoreSequence){
 		this->scoreSequence = scoreSequence;
 		computeScore();
