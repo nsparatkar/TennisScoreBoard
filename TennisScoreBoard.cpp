412d93a4ca1a0459f1e25c7f973aeeddb2164228
dineshchhabra <test@L-MAA-25004134.paypalcorp.com>
2015-07-27 13:42:53 +0530

    Adding won() and correcting if condition in checkAdvantageLost

--- TennisScoreBoard.cpp
+++ TennisScoreBoard.cpp
@@ -7,49 +7,49 @@ class TennisScoreBoard{
 	int score_D;
 	int score_F;
 	string displayedScore;
-	const string score[5] = {"0", "15", "30", "40","A"};
+	const string score[6] = {"0", "15", "30", "40","A"," "};
 
-	
-
-	bool checkAdvantageLost(int i){
-		return (score_D == 4 && scoreSequence.at(i) == 'F') || (score_F == 4 && scoreSequence.at(i) == 'D') ;
-	}
-
-	bool playerAdvantage() {
-		if( (score_D + score_F) == 7 )
-			return true;
-		return false;
+	void checkAdvantageLost(int i){
+		if (score_D == 4  && score_F == 4 ) {
+		    score_F--;
+			score_D--;
+		}
 	}
 
 	void updateScoreBoard(){
 
-		if(playerAdvantage()) {
-			if(score_D > score_F)
-				displayedScore = "A - ";
-			else
-				displayedScore = " - A";
-		} else if(score_D == 5 || score_F == 5) {
-				displayedScore = "Game";
-		} else {
-			displayedScore += score[score_D];
-			displayedScore += " ";
-			displayedScore += score[score_F];
-		}
+        displayedScore += score[score_D];
+        displayedScore += " ";
+        displayedScore += score[score_F];
 	}
 
+    bool won(){
+        if ((score_D-score_F >=2) && (score_D >=4)) {
+            displayedScore = "Game to D";
+            return true;
+        }
+        else if ((score_F-score_D >=2) && (score_F >=4)) {
+            displayedScore = "Game to F";
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 	void computeScore(){
-		for (int i = 0; i < scoreSequence.length(); i++) {
+		for(int i = 0; i < scoreSequence.length(); i++){
 			displayedScore = "";
-			if (scoreSequence[i] == 'D') {
+			if(scoreSequence[i] == 'D'){
 				score_D++;
-			} else {
+			}else{
 				score_F++;
 			}
-			if( checkAdvantageLost(i) ){
-				score_D--;
-				score_F--;
-			}
+			checkAdvantageLost(i);
 			updateScoreBoard();
+			if (won()) {
+			    return;
+			}
 		}
 	}
 
@@ -71,7 +71,7 @@ class TennisScoreBoard{
 
 int main()
 {
-    string sequenceOfPoints = "DFDFDFDFDD";
+    string sequenceOfPoints = "DFDFDFDF";
 
     TennisScoreBoard tennisGame;
     tennisGame.displayScore(sequenceOfPoints);
