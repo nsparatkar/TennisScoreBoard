d343077ca9e7441645fd59088c8331b48ab56054
nsparatkar <nsparatkar@gmail.com>
2015-07-27 11:11:00 +0530

    Added the computeScore and updateScoreBoard functions

--- TennisScoreBoard.cpp
+++ TennisScoreBoard.cpp
@@ -7,20 +7,42 @@ class TennisScoreBoard{
 	int score_D;
 	int score_F;
 	string score[] = {"0", "15", "30", "40"};
+	string displayedScore;
 
 	TennisScoreBoard(){
 		scoreSequence = "";
 		score_D = 0;
 		score_F = 0;
+		displayedScore = "0 - 0";
 	}
 	
-	string computeScore(){
-		
+	void updateScoreBoard(){
+		displayScore += score[score_D];
+		displayScore += " ";
+		displayScore += score[score_F];
+	}
+	
+	void computeScore(){
+		for(char scorer : scoreSequence){
+			displayedScore = "";
+			if(scorer == 'D'){
+				score_D++;
+			}else{
+				score_F++;
+			}
+			if( scoreDuece() ){
+				score_D--;
+				score_F--;
+			}
+			updateScoreBoard();
+		}
 	}
 	
 	public:
 	void displayScore(string scoreSequence){
 		this->scoreSequence = scoreSequence;
+		computeScore();
+		cout << displayedScore;
 	}
 	
 };
